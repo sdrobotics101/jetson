ac488b7c9d2dfc50349014b6af30a7d625932895
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2015-06-14 12:06:47 -0700

    Added changing packet to networkserver_test
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- src/experiments/networkserver_test.cpp
+++ src/experiments/networkserver_test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "NetworkServer.hpp"
 #include "N2MStandardPacket.hpp"
@@ -9,8 +10,16 @@ int main(int argc, char* argv[])
     NetworkServer server;
     server.open();
     server.start();
-    server.get_n2m_standard_packet()->set_vel_x(123.45);
-    while (1);
+    for (;;)
+    {
+        server.get_n2m_standard_packet()->set_vel_x(server.get_n2m_standard_packet()->get_vel_x() + 1.0);
+        server.get_n2m_standard_packet()->set_vel_y(server.get_n2m_standard_packet()->get_vel_y() + 2.0);
+        server.get_n2m_standard_packet()->set_pos_z(server.get_n2m_standard_packet()->get_pos_z() + 3.0);
+        server.get_n2m_standard_packet()->set_rot_x(server.get_n2m_standard_packet()->get_rot_x() + 4.0);
+        server.get_n2m_standard_packet()->set_rot_y(server.get_n2m_standard_packet()->get_rot_y() + 5.0);
+        server.get_n2m_standard_packet()->set_rot_z(server.get_n2m_standard_packet()->get_rot_z() + 6.0);
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
     
     return EXIT_SUCCESS;
 }
