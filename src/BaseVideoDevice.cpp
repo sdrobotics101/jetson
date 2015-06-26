757ce736e5fc739fcd29c820f61db78c4c9a4a74
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2015-06-25 19:33:06 -0700

    Fixed licensing
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- src/BaseVideoDevice.cpp
+++ src/BaseVideoDevice.cpp
@@ -1,23 +1,3 @@
-/* BaseVideoDevice.cpp -- Implementation of BaseVideoDevice class
-
-   Copyright (C) 2012, 2013, 2014 Tushar Pankaj
-   
-   This file is part of San Diego Robotics 101 Robosub.
-   
-   San Diego Robotics 101 Robosub is free software: you can redistribute it
-   and/or modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation, either version 3 of the License,
-   or (at your option) any later version.
-   
-   San Diego Robotics 101 Robosub is distributed in the hope that it will be
-   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-   Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with San Diego Robotics 101 Robosub.  If not, see
-   <http://www.gnu.org/licenses/>. */
-
 #include <opencv2/opencv.hpp>
 #include "BaseVideoDevice.hpp"
 
